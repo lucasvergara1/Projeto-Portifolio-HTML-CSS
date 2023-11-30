016dcc5824187e66913d52848fccb62a572d4fdd
lucasvergara1 <lucas.vergara@live.com>
2023-11-30 00:23:05 -0300

    Create JS doc and its instructions

--- js/components/main-carousel.js
+++ js/components/main-carousel.js
@@ -1,1 +1,16 @@
-alert("Caixa troll")
+//usado para fazer comentarios no JS
+//Queremos fazer mover o carousel, logica por de trás 
+
+//1 Acessar a Janela (browser)
+//2 Pegar HTML 
+//3 Pegar botão
+//4 Saber que está sendo clicado no botão
+
+//5 Acessar a Janela
+//6 PEgar HTML todo
+//7 Pegar o elements
+//8 Mover o Elements
+
+const btnRight = window.document.querySelector('.button-arrow.-right')
+
+console.log(btnRight);
