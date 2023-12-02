4ac69a84a79be047110c3e59f60db78113376837
lucasvergara1 <lucas.vergara@live.com>
2023-12-01 21:45:08 -0300

    Create main label and form

--- js/components/main-carousel.js
+++ js/components/main-carousel.js
@@ -16,15 +16,15 @@
 const btnRight = document.querySelector('.button-arrow.-right');
 const btnLeft = document.querySelector('.button-arrow.-left');
 const elements = document.querySelector('.elements');
-let pixels = 10;
+let pixels = 50;
 
 btnRight.addEventListener('click', function() {
-    pixels = pixels + 10;
+    pixels = pixels + 50;
     elements.style.transform = `translateX(${pixels}px)`;
 });
 
 btnLeft.addEventListener('click', function(){
-    pixels = pixels - 10;
+    pixels = pixels - 50;
     elements.style.transform = `translateX(${pixels}px)`;
 
 });
